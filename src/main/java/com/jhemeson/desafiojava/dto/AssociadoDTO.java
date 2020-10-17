1a1d259276bfd0e681b5a3cacc295b31aa76c143
Jhemeson Mota <jhemeson.mota@wipro.com>
2020-10-17 16:35:46 -0300

    service de associado

--- src/main/java/com/jhemeson/desafiojava/dto/AssociadoDTO.java
+++ src/main/java/com/jhemeson/desafiojava/dto/AssociadoDTO.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AssociadoDTO {
+    private Long id;
     private String cpf;
     private String nome;
 }
--- src/main/java/com/jhemeson/desafiojava/entity/Associado.java
+++ src/main/java/com/jhemeson/desafiojava/entity/Associado.java
@@ -14,6 +14,9 @@ import javax.persistence.*;
 @AllArgsConstructor
 public class Associado {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(nullable = false, unique = true)
     private String cpf;
 
