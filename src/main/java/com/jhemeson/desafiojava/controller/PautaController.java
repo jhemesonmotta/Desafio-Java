3cb14328011d9f20b560515017a7236e0a75be81
Jhemeson Mota <jhemeson.mota@wipro.com>
2020-10-17 17:35:33 -0300

    criar pauta funcionando

--- src/main/java/com/jhemeson/desafiojava/controller/PautaController.java
+++ src/main/java/com/jhemeson/desafiojava/controller/PautaController.java
@@ -1,10 +1,29 @@
 package com.jhemeson.desafiojava.controller;
 
+import com.jhemeson.desafiojava.dto.MessageResponseDTO;
+import com.jhemeson.desafiojava.dto.PautaDTO;
+import com.jhemeson.desafiojava.service.PautaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/pauta")
 public class PautaController {
+
+    private PautaService pautaService;
+
+    @Autowired
+    public PautaController(PautaService pautaService) {
+        this.pautaService = pautaService;
+    }
+
     // CADASTRAR PAUTA
+    @PostMapping
+    public MessageResponseDTO create(@RequestBody PautaDTO pautaDTO) {
+        return pautaService.create(pautaDTO);
+    }
+
 }
