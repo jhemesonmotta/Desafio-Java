767752e5320a33e6dd02927fedd0ad3e73a1f708
Jhemeson Mota <jhemeson.mota@wipro.com>
2020-10-17 16:20:37 -0300

    pauta service pronto

--- src/main/java/com/jhemeson/desafiojava/service/PautaService.java
+++ src/main/java/com/jhemeson/desafiojava/service/PautaService.java
@@ -1,7 +1,35 @@
 package com.jhemeson.desafiojava.service;
 
+import com.jhemeson.desafiojava.dto.MessageResponseDTO;
+import com.jhemeson.desafiojava.dto.PautaDTO;
+import com.jhemeson.desafiojava.entity.Pauta;
+import com.jhemeson.desafiojava.mapper.PautaMapper;
+import com.jhemeson.desafiojava.repository.PautaRepository;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PautaService {
+    private PautaRepository pautaRepository;
+    private static final PautaMapper pautaMapper = PautaMapper.INSTANCE;
+
+    @Autowired
+    public PautaService(PautaRepository pautaRepository) {
+        this.pautaRepository = pautaRepository;
+    }
+
+    public MessageResponseDTO create(PautaDTO pautaDTO) {
+        Pauta pautaToCreate = pautaMapper.toModel(pautaDTO);
+        Pauta pautaCreated = pautaRepository.save(pautaToCreate);
+
+        return MessageResponseDTO.builder().
+                message("Pauta " + pautaCreated.getNome() + " foi adicionada à base de dados com o ID:" + pautaCreated.getId())
+                .build();
+    }
+
+    public PautaDTO findById(Long id) throws NotFoundException {
+        Pauta pauta = pautaRepository.findById(id).orElseThrow(() -> new NotFoundException("Pauta não encontrada."));
+        return pautaMapper.toDTO(pauta);
+    }
 }
